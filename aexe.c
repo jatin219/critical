5f5ceaf2f4421cd5bb3bdd3c4d7998806bd46ce9
jatin219 <jt.sharma@outlook.com>
2024-12-28 14:28:21 +0530

    Create aexe.c

---
+++ aexe.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int mian()
+{
+  printf("\n happy new year 2025");
+  printf("\n may god fullfill your all desire whishes");
+  printf("\n may your delulu become trululu");
+  printf("\n bye bye 2024");
+  printf("\n ENJOY....");
+  return 0;
+}
